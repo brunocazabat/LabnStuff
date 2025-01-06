afe89e752c968a47e66a6fefa3b1460fd04e999a
Bruno Cazabat <bruno.cazabat@gmail.com>
2025-01-06 07:18:22 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-adduser "$1" <<EOF
+adduser "$0" <<EOF
 $password
 $password
 EOF
 
-adduser "$1" sudo && adduser "$1" docker
+adduser "$0" sudo && adduser "$0" docker
 
-sed -i -e "s/#Port 22/Port $2/g" /etc/ssh/sshd_config && systemctl restart sshd
+sed -i -e "s/#Port 22/Port $1/g" /etc/ssh/sshd_config && systemctl restart sshd
 apt update && apt upgrade -y && apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common net-tools
 curl -sSL https://get.docker.com/ | bash
 {

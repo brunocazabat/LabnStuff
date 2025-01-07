baeb31ac890c4a631c7b7bf871ed4f7197643045
Bruno Cazabat <bruno.cazabat@gmail.com>
2025-01-07 15:18:57 +0100

    feat: update of installed package

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@
 adduser --quiet --disabled-password bruno && sleep 2 && passwd bruno && sleep 2 && adduser bruno sudo && groupadd docker && adduser bruno docker
 
 sed -i -e "s/#Port 22/Port 1999/g" /etc/ssh/sshd_config && systemctl restart sshd
-apt update && apt upgrade -y && apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common net-tools
+apt update && apt upgrade -y && apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common net-tools htop
 curl -sSL https://get.docker.com/ | bash
 {
     echo "vm.swappiness=10"

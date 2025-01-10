dbe28711c56620abddfd2cbbbfc6c8c9cf41b3b5
Bruno Cazabat <bruno.cazabat@gmail.com>
2025-01-10 23:51:54 +0100

    Create LXC-23.10-to-24.04.bash

---
+++ LXC-23.10-to-24.04.bash
@@ -0,0 +1,5 @@
+# This script is used to update an Ubuntu 23.10 container to 24.04
+# DO NOT USE THIS SCRIPT ON A PRODUCTION CONTAINER WITHOUT BACKUP OR ON A CONTAINER THAT YOU CAN'T LOSE
+# DO NOT USE THIS SCRIPT ON A CONTAINER THAT IS NOT USING UBUNTU 23.10 (NOW EOL)
+
+bash -c "$(wget -qO - https://raw.githubusercontent.com/brunocazabat/LabnStuff/refs/heads/main/update.sh)"

8d833a3878365fbdea2a7b97f03b0be18cb359ea
Bruno Cazabat <bruno.cazabat@gmail.com>
2025-01-10 23:56:46 +0100

    feat: update of bash scripts for ptero wings

--- 22-Pterodactyl-wing1/LXC-docker.bash
+++ 22-Pterodactyl-wing1/LXC-docker.bash
@@ -1,15 +1,11 @@
-# From "Mom"
-lxc-attach --name "id"
-# From "Dad"
-sudo passwd root
-nano /etc/ssh/sshd_config
-# PermitRootLogin prohibit-password > PermitRootLogin yes
-systemctl restart sshd
+# This script is used to install docker on a LXC container and do the initial setup
+# A variable with "bruno" is fixed in the script, you must change it to your username instead.
 
-apt update && apt upgrade -y && apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common net-tools ufw
 
-curl -sSL https://get.docker.com/ | bash
+bash -c "$(wget -qO - https://raw.githubusercontent.com/brunocazabat/LabnStuff/refs/heads/main/init.sh)"
 
+
+# EXTRA CMDS
 ufw allow ssh
 ufw allow 8080
 ufw allow 2022

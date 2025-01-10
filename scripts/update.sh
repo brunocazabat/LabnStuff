b47009eef7f28caba5138ebda351610a1d496082
Bruno Cazabat <bruno.cazabat@gmail.com>
2025-01-11 00:21:27 +0100

    test

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sed -i -e "s/'http://archive.ubuntu.com/ubuntu/'/'http://old-releases.ubuntu.com/ubuntu/'/g" /etc/apt/sources.list
+sed -i "s|http://archive.ubuntu.com/ubuntu|http://old-releases.ubuntu.com/ubuntu|g" /etc/apt/sources.list
+
+# can I replace
 
 apt update && apt upgrade -y && do-release-upgrade

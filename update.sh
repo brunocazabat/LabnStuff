3df1f5bd5f1385750dd37885a669341239921b77
Bruno Cazabat <bruno.cazabat@gmail.com>
2025-01-10 23:50:21 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sed -i -e "s/'http://archive.ubuntu.com/ubuntu/'/'http://old-releases.ubuntu.com/ubuntu/'/g" /etc/apt/sources.list
+
+apt update && apt upgrade -y && do-release-upgrade -d
+

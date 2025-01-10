85036fac55bc305c0d0ac4661347f5c64b4915ed
Bruno Cazabat <bruno.cazabat@gmail.com>
2025-01-10 22:15:30 +0100

    feat: removing duplicates

--- 5-homepage/LXC-docker.bash
+++
@@ -1,3 +0,0 @@
-# This script is used to install docker on a LXC container and do the initial setup
-
-bash -c "$(wget -qO - https://raw.githubusercontent.com/brunocazabat/LabnStuff/refs/heads/main/init.sh)"

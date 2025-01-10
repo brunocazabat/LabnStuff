216abc821ea122f94dd3918c1b828be94ef8e551
Bruno Cazabat <bruno.cazabat@gmail.com>
2025-01-10 23:59:30 +0100

    feat: adapt to new script path

--- LXC-23.10-to-24.04.bash
+++ LXC-23.10-to-24.04.bash
@@ -2,4 +2,4 @@
 # DO NOT USE THIS SCRIPT ON A PRODUCTION CONTAINER WITHOUT BACKUP OR ON A CONTAINER THAT YOU CAN'T LOSE
 # DO NOT USE THIS SCRIPT ON A CONTAINER THAT IS NOT USING UBUNTU 23.10 (NOW EOL)
 
-bash -c "$(wget -qO - https://raw.githubusercontent.com/brunocazabat/LabnStuff/refs/heads/main/update.sh)"
+bash -c "$(wget -qO - https://raw.githubusercontent.com/brunocazabat/LabnStuff/refs/heads/main/scripts/update.sh)"
--- LXC-docker.bash
+++ LXC-docker.bash
@@ -1,3 +1,5 @@
 # This script is used to install docker on a LXC container and do the initial setup
+# A variable with "bruno" is fixed in the script, you must change it to your username instead.
 
-bash -c "$(wget -qO - https://raw.githubusercontent.com/brunocazabat/LabnStuff/refs/heads/main/init.sh)"
+
+bash -c "$(wget -qO - https://raw.githubusercontent.com/brunocazabat/LabnStuff/refs/heads/main/scripts/init.sh)"
